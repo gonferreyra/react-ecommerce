4ca1c0a77b4309db7f5c9022251fe8c8e2806505
Gonzalo Ferreyra <gonferreyra@gmail.com>
2022-10-14 11:07:32 -0300

    moving database to state in reducer

--- src/redux/Shopping/shopping-reducer.js
+++ src/redux/Shopping/shopping-reducer.js
@@ -29,6 +29,141 @@ const INITIAL_STATE = {
             url: 'air-jordan-1-low-flyease',
             info: "Lock in your style with this AJ1. We kept everything you love about the classic design—premium leather, Air cushioning, iconic Wings logo—while adding the Nike FlyEase closure system to make on and off a breeze. Getting out the door is now quicker than ever: just strap and zip."
         },
+        {
+            id: '4',
+            name: 'Air Jordan 1 Mid SE',
+            category: 'Jordan',
+            price: 135,
+            stock: 2,
+            url: 'air-jordan-1-mid-se',
+            info: "Stand out with timeless hoops style. Premium leather gets polished, shined and textured to deliver a fresh take on the shoe that started it all. Air cushioning and a classic design keep everything you love best from the original."
+        },
+        {
+            id: '5',
+            name: 'Air Jordan 1 Mid',
+            category: 'Jordan',
+            price: 120,
+            stock: 2,
+            url: 'air-jordan-1-mid',
+            info: "Inspired by the original AJ1, the Air Jordan 1 Mid offers fans a chance to follow in MJ's footsteps. Fresh color trims the clean, classic materials, imbuing modernity into a classic design."
+        },
+        {
+            id: '6',
+            name: 'Jordan 1 Mid SE',
+            category: 'Jordan',
+            price: 80,
+            stock: 2,
+            url: 'air-jordan-1-mid-',
+            info: "With classic colors and a time-tested design, the Jordan 1 is made for all day play. It's soft and flexible underfoot, easy to get on, and comfortable for little feet."
+        },
+        {
+            id: '7',
+            name: 'NikeCourt Vapor Cage 4 Rafa',
+            category: 'Tenis',
+            price: 120,
+            stock: 2,
+            url: 'nikecourt-zoom-vapor',
+            info: "Innovated to withstand your toughest matches, this updated design puts flexible, durable materials exactly where they're needed most. Signature Rafael Nadal details let you rep your favorite player while you run the court."
+        },
+        {
+            id: '8',
+            name: 'NikeCourt Zoom NXT',
+            category: 'Tenis',
+            price: 140,
+            stock: 2,
+            url: 'nikecourt-zoom-nxt',
+            info: "Get the most out of every step in the NikeCourt Zoom NXT. Using insights from our top athletes, we added forefoot cushioning for extra bounce and spring when you push off to serve. After removing as much weight as we could without sacrificing durability, we also made sure this incredibly lightweight design was extremely comfortable. Thanks to a plush collar and breathable mesh, you'll get the cool, cushioned feel you love so you can focus on playing at your best."
+        },
+        {
+            id: '9',
+            name: 'NikeCourt Air Zoom Vapor Pro',
+            category: 'Tenis',
+            price: 120,
+            stock: 2,
+            url: 'nikecourt-air-zoom-vapor-pro',
+            info: "The NikeCourt Air Zoom Vapor Pro takes everything you love about the NikeCourt Air Zoom Vapor X and makes it lighter and more supportive. The 3-layer design puts breathability, stability and durability where they're needed most, all while keeping weight at a minimum."
+        },
+        {
+            id: '10',
+            name: 'NikeCourt Vapor Lite',
+            category: 'Tenis',
+            price: 80,
+            stock: 2,
+            url: 'nikecourt-vapor-lite',
+            info: "Like a race car, the NikeCourt Vapor Lite keeps most of its features under the hood. Its unique cushioning system is built to help you get to the ball as fast as you can. An innovative, 4-point strap takes learnings from race car harnesses to help keep your foot locked down during your fiercest matches. All in a lightweight, breathable design that's incredibly comfortable."
+        },
+        {
+            id: '11',
+            name: 'NikeCourt Zoom Pro',
+            category: 'Tenis',
+            price: 120,
+            stock: 2,
+            url: 'nikecourt-zoom-pro',
+            info: "Harness the power of your serve in the NikeCourt Zoom Pro. Working in tandem with the Zoom Air unit in the forefoot, it has a full-length plate that acts like a springboard. Flexing with every step, it helps enhance energy return and increase stability. With breathable materials, this responsive design helps keep you feeling fresh throughout your toughest matches."
+        },
+        {
+            id: '12',
+            name: 'NikeCourt Zoom Lite 3',
+            category: 'Tenis',
+            price: 70,
+            stock: 2,
+            url: 'nikecourt-zoom-lite-3',
+            info: "At first glance, the NikeCourt Zoom Lite 3 looks like a classic tennis shoe. Look closer and you'll find that it's anything but traditional. With a springy Zoom Air unit in the forefoot, a grippy outsole and an incredibly comfortable padded collar, it delivers the performance you need to play at your best."
+        },
+        {
+            id: '13',
+            name: 'Nike Phantom GT2 Elite FG',
+            category: 'Football',
+            price: 250,
+            stock: 2,
+            url: 'nike-phantom-gt2-elite-fg',
+            info: "Building off the Phantom GT, the Nike Phantom GT2 Elite FG features an updated design and raised patterning to help create optimal spin to control the flight of the ball. Off-center lacing provides a clean strike zone for skillful dribbling, passing and shooting."
+        },
+        {
+            id: '14',
+            name: 'Nike Mercurial Superfly 9 Elite',
+            category: 'Football',
+            price: 275,
+            stock: 2,
+            url: 'nike-zoom-mercurial-superfly',
+            info: "Instantly tilt the field in the bold design of the Superfly 9 Elite FG. We added a Zoom Air unit, made specifically for soccer, and grippy texture up top for exceptional touch, so you can dominate in the waning minutes of a match—when it matters most. Feel the explosive speed as you race around the field, making the critical plays with velocity and pace. Fast is in the Air."
+        },
+        {
+            id: '15',
+            name: 'Nike Mercurial Superfly 8 Elite',
+            category: 'Football',
+            price: 275,
+            stock: 2,
+            url: 'nike-zoom-mercurial-superfly-8-elite',
+            info: "Unlock the blueprint for speed with the Nike Mercurial Superfly 8 Elite FG. Color blocking at the sides and forefoot highlights key areas for precise dribbling, passing and shooting. The innovative plate provides instant responsiveness for quicker cuts at high speeds."
+        },
+        {
+            id: '16',
+            name: 'Nike Tiempo Legend 9 Pro FG',
+            category: 'Football',
+            price: 130,
+            stock: 2,
+            url: 'nike-tiempo-legend-9-pro',
+            info: "1 of our lightest Tiempos to date, the Nike Tiempo Legend 9 Pro FG lets you go on the offensive with a low-profile design that's reinvented for attackers. The upper has raised textures backed by soft foam pods for precise dribbling, passing and shooting, while studs on the bottom provide traction for quick cuts and sudden stops."
+        },
+        {
+            id: '17',
+            name: 'Nike Tiempo Legend 9 Academy',
+            category: 'Football',
+            price: 75,
+            stock: 2,
+            url: 'nike-tiempo-legend-9-academy',
+            info: "1 of our lightest Tiempos to date, the Nike Tiempo Legend 9 Academy MG lets you go on the offensive with a low-profile design that's reinvented for attackers. The upper has raised textures backed by soft fsoam pods for precise dribbling, passing and shooting, while studs on the bottom provide traction for quick cuts and sudden stops."
+        },
+        {
+            id: '18',
+            name: 'Nike Phantom GT2 Academy',
+            category: 'Football',
+            price: 80,
+            stock: 2,
+            url: 'nike-phantom-gt2-academy',
+            info: "Building on the Phantom GT, the Nike Phantom GT2 MG has an updated design and patterning that are engineered to help you place your shots with pinpoint accuracy. Off-center lacing provides a clean strike zone to help you dribble, pass and score with precision."
+        }
     ],
     cart: [],
     currentItem: null,
