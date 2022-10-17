212d8f742ef6a3dab0f7a3923e4724b45f18560d
Gonzalo Ferreyra <gonferreyra@gmail.com>
2022-10-17 12:37:09 -0300

    ScrollToTop function and product component added

--- src/components/router/RouterApp.js
+++ src/components/router/RouterApp.js
@@ -1,10 +1,14 @@
 import { useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ScrollToTop from '../../helpers/ScrollToTop'
 import Home from '../../pages/Home'
+import Cart from '../Cart/Cart'
 import { UserContext } from '../Context/UserContext'
+import Footer from '../Footer/Footer'
 import Login from '../Login/Login'
 import Navbar from '../Navbar/Navbar'
 import Sidebar from '../Sidebar/Sidebar'
+import SneakerItem from '../SneakersSection/SneakerItem/SneakerItem'
 
 const RouterApp = () => {
 
@@ -12,13 +16,17 @@ const RouterApp = () => {
 
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <Navbar isOpen={isOpen} toggle={toggle} />
+            <Cart />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="*" element={<Home />} />
+                <Route path='/product/:id' element={<SneakerItem />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,14 @@ import Hero from "../components/Hero/Hero";
 import NewReleaseSection from "../components/NewReleaseSection/NewReleaseSection";
 import SneakersSection from "../components/SneakersSection/SneakersSection";
 import ServicesSection from "../components/ServicesSection/Services";
-import Footer from "../components/Footer/Footer";
-import Cart from "../components/Cart/Cart";
 
 const Home = () => {
   return (
     <>
-      <Cart />
       <Hero />
       <SneakersSection />
       <ServicesSection />
       <NewReleaseSection />
-      <Footer />
     </>
   );
 };
