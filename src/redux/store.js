76a9f7a792573a79439f139b671404964ce1ebc1
Gonzalo Ferreyra <gonferreyra@gmail.com>
2022-10-20 11:05:58 -0300

    replacing createStore with configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
-import { legacy_createStore as createStore } from 'redux';
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store = configureStore({
+    reducer: rootReducer,
+
+})
 
 export default store
