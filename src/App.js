d58bbc7135800c3d19872b048ec76cb92db486e6
Gonzalo Ferreyra <gonferreyra@gmail.com>
2022-11-10 18:29:26 -0300

    localstorage to cart items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import './App.css'
 import { UserContext } from "./components/Context/UserContext"
 import RouterApp from './router/RouterApp';
 
 function App() {
+
+  const cart = useSelector(state => state.shop.cart)
+  // console.log(cart)
+
+  useEffect(() => {
+    localStorage.setItem('savedCart', JSON.stringify(cart))
+  }, [cart])
+
   // navbar-aside
   const [isOpen, setIsOpen] = useState(false);
 
